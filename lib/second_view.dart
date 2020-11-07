39065cb2f0fdb7f644187775315aa03611f70034
Chuyen Trung Tran <chuyentt@gmail.com>
2020-11-07 08:20:17 +0700

    Hướng dẫn tạo readme với markdown

---
+++ lib/second_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SecondView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second')),
+      body: Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
