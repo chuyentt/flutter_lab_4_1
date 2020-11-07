0fe3e1d8459e0c6759495cff0244b367c02c09ec
Chuyen Trung Tran <chuyentt@gmail.com>
2020-11-07 08:20:39 +0700

    Thêm mới views

---
+++ lib/first_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First')),
+      body: Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
---
+++ lib/main_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main')),
+      body: Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
